09eba930df6edd874a67900edc9b3f773b00dadf
Oliver Scott <olivercscott@gmail.com>
2023-07-20 10:04:19 -0400

    calyxos: Add CalyxOS specific configuration
    * Change name and description to CalyxOS
    
    Change-Id: Ie0f6adcf46a330f01ca4ebfb316b9a74ec4dac1d

--- src/config.js
+++ src/config.js
@@ -1,86 +1,23 @@
 export default {
     OS_NAME: import.meta.env.VITE_APP_OS_NAME,
     ACCENT_COLOR: "#0366d6",
-    SUPPORTED_DEVICES: [
-        {
-            name: "Pixel 3",
-            model: "blueline",
-        },
-        {
-            name: "Pixel 3 XL",
-            model: "crosshatch",
-        },
-        {
-            name: "Pixel 3a",
-            model: "sargo",
-        },
-        {
-            name: "Pixel 3a XL",
-            model: "bonito",
-        },
-        {
-            name: "Pixel 4",
-            model: "flame",
-        },
-        {
-            name: "Pixel 4 XL",
-            model: "coral",
-        },
-        {
-            name: "Pixel 4a",
-            model: "sunfish",
-        },
-        {
-            name: "Pixel 5",
-            model: "redfin",
-        },
-        {
-            name: "Pixel 4a (5G)",
-            model: "bramble",
-        },
-        {
-            name: "Pixel 5a (5G)",
-            model: "barbet",
-        },
-        {
-            name: "Pixel 6",
-            model: "oriole",
-        },
-        {
-            name: "Pixel 6 Pro",
-            model: "raven",
-        },
-        {
-            name: "Pixel 6a",
-            model: "bluejay",
-        },
-        {
-            name: "Pixel 7",
-            model: "panther",
-        },
-        {
-            name: "Pixel 7 Pro",
-            model: "cheetah",
-        },
-        {
-            name: "Fairphone 4",
-            model: "FP4",
-        },
-    ],
     RELEASE_VARIANTS: {
-        stable: {
-            description: "Stable release.",
+        minimal: {
+            description: "Minimal version without Google apps or services.",
             suffix: "",
         },
-    },
-    DONATION_LINKS: [
-        {
-            title: "Make a Tax-deductible Donation to Calyx Institute",
+        gapps: {
             description:
-                "You can donate through paypal, cryptocurrency, or by mail.",
-            highlight: false,
-            url: "https://members.calyxinstitute.org/donate",
-            icon: "",
+                "Version with Google services included, so the apps you’re familiar with will work. Recommended for most users.",
+            suffix: " with Google services",
         },
-    ],
+        test: {
+            description: "Experimental version for testing only.",
+            suffix: "experimental",
+        },
+        beta: {
+            description: "Beta version for testing only.",
+            suffix: "beta",
+        },
+    },
 };
