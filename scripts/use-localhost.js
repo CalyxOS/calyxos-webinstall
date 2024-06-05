fedfb367f586167887f7f06c66f197e790cfbce0
ziggy <ziggy@calyxinstitute.org>
2024-06-05 14:36:36 -0400

    always download and use latest factory release
    Change-Id: I60095c70c1da3cb4198efbcebfe8d29c349c9175

---
+++ scripts/use-localhost.js
@@ -0,0 +1,14 @@
+// updates urls in public/releases/index.json to use localhost/calyxos
+import path from "path"
+import fs from "fs"
+import { fileURLToPath } from 'url'
+
+const base_path = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..')
+const releases_dest_path = path.resolve(base_path, 'public', 'releases', 'index.json')
+const data = JSON.parse(fs.readFileSync(releases_dest_path))
+
+for (let [key, value] of Object.entries(data)) {
+  value['url'] = value['url'].replace('release.calyxinstitute.org', 'localhost/calyxos')
+}
+
+fs.writeFileSync(releases_dest_path, JSON.stringify(data, null, "  "))
