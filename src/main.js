cd5f795e23748de5b15954579ba023a580d696fe
ziggy <ziggy@calyxinstitute.org>
2024-06-18 16:39:41 -0400

    remove unused dependencies sentry & webpack
    Change-Id: I724751f8694f30ef82c8a47f09fb6f0e394551ab

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from "./App.vue";
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import * as labsComponents from 'vuetify/labs/components'
-import * as Sentry from "@sentry/vue";
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
-import { Integrations } from "@sentry/tracing";
 import 'vuetify/styles'
 
 const vuetify = createVuetify({
@@ -18,20 +16,6 @@ const vuetify = createVuetify({
 })
 
 const app = createApp(App).use(vuetify).mount('#app')
-
-if (
-    import.meta.env.VITE_SENTRY_DSN !== undefined &&
-    import.meta.env.NODE_ENV === "production"
-) {
-    Sentry.init({
-        app,
-        dsn: process.env.VITE_SENTRY_DSN,
-        integrations: [new Integrations.BrowserTracing()],
-        tracesSampleRate: 1.0,
-        attachProps: true,
-        logErrors: true,
-    });
-}
 
 if (process.env.NODE_ENV !== 'production' && typeof(window) !== 'undefined') {
   window.$app = app;
