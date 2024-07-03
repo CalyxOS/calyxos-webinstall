fe77cbd1d703664886a7340d8a74b0eec99abe56
ziggy <ziggy@calyxinstitute.org>
2024-07-03 16:57:13 -0400

    Show notice on Connect step when web_install is false
    Change-Id: I9ad5aa487dbf9b7eee13ff066724e918bc9cf113

--- src/components/ConnectStep.vue
+++ src/components/ConnectStep.vue
@@ -1,57 +1,66 @@
 <template>
-    <v-container class="d-flex justify-space-between flex-column flex-grow-1">
-        <div class="mb-10 mt-n4">
-            <h6 class="text-h6 pb-4">Connect your device</h6>
+<v-container class="d-flex justify-space-between flex-column flex-grow-1">
+  <div class="mb-10 mt-n4">
+    <h6 class="text-h6 pb-4">Connect your device</h6>
 
-            <div class="text-body-1 mb-4">
-                <p>
-                    Put your device into bootloader mode by restarting it and
-                    holding the
-                    <strong>volume down</strong> button until you see a
-                    <span class="text-red-darken-2">red</span> warning
-                    sign or
-                    <span class="text-green-darken-2">green</span>
-                    Android robot.
-                </p>
-                <p class="mt-1">
-                    Once your device is in bootloader mode, plug it into the
-                    computer or device you’re installing from.
-                </p>
-                <p class="mt-1">
-                    Make sure you use a <strong>high-quality</strong> USB cable,
-                    as many cables will cause issues. Avoid USB hubs if possible.
-                    Your USB cable needs to be able to copy files. Charging-only
-                    cables won’t work.
-                </p>
-            </div>
+    <div class="text-body-1 mb-4">
+      <p>
+        Put your device into bootloader mode by restarting it and
+        holding the
+        <strong>volume down</strong> button until you see a
+        <span class="text-red-darken-2">red</span> warning
+        sign or
+        <span class="text-green-darken-2">green</span>
+        Android robot.
+      </p>
+      <p class="mt-1">
+        Once your device is in bootloader mode, plug it into the
+        computer or device you’re installing from.
+      </p>
+      <p class="mt-1">
+        Make sure you use a <strong>high-quality</strong> USB cable,
+        as many cables will cause issues. Avoid USB hubs if possible.
+        Your USB cable needs to be able to copy files. Charging-only
+        cables won’t work.
+      </p>
+    </div>
 
-            <v-btn
-                :color="$root.$data.product === null ? 'primary' : null"
-                @click="connect"
-                :disabled="connecting"
-                >Connect</v-btn
-            >
-        </div>
+    <v-btn
+      :color="$root.$data.product === null ? 'primary' : null"
+      @click="connect"
+      :disabled="connecting"
+      text="Connect"
+      ></v-btn>
+  </div>
 
-        <div class="mb-4">
-            <connect-banner
-                :device="device"
-                :connecting="connecting"
-                :error="error"
-                :name="getDeviceName()"
-            />
-        </div>
+  <div class="mb-4 mt-n4" v-if="$root.$data.product && !connecting && !installable()">
+    <v-alert density="compact" title="Device Not Supported" type="tonal">
+      <p>
+        We're sorry, your device cannot be installed using the web installer.<br/>
+        Visit <a href="https://calyxos.org/install/">calyxos.org/install</a> for more installation methods.
+      </p>
+    </v-alert>
+  </div>
 
-        <div class="d-flex justify-space-between flex-row-reverse">
-            <v-btn
-                color="primary"
-                @click="emit('nextStep')"
-                :disabled="$root.$data.product === null"
-                >Next <v-icon dark right>mdi-arrow-right</v-icon></v-btn
-            >
-            <v-btn text @click="emit('prevStep')">Back</v-btn>
-        </div>
-    </v-container>
+  <div class="mb-4">
+    <connect-banner
+      :device="device"
+      :connecting="connecting"
+      :error="error"
+      :name="getDeviceName()"
+      />
+  </div>
+
+  <div class="d-flex justify-space-between flex-row-reverse">
+    <v-btn
+      color="primary"
+      @click="emit('nextStep')"
+      :disabled="$root.$data.product === null || !installable()"
+      >Next <v-icon dark right>mdi-arrow-right</v-icon></v-btn
+                                                         >
+    <v-btn text @click="emit('prevStep')">Back</v-btn>
+  </div>
+</v-container>
 </template>
 
 <script>
@@ -89,7 +98,10 @@ export default {
 
                 if (this.firstConnect) {
                     this.firstConnect = false;
-                    this.emit("nextStep");
+                    if (this.installable()) {
+                        this.emit("nextStep");
+                    }
+
                 }
 
                 this.saEvent(`device_connect__${this.$root.$data.product}`);
@@ -105,17 +117,21 @@ export default {
         },
 
         getDeviceName() {
-          let product = this.$root.$data.product;
-          if (!product) {
-              return '';
-          }
-          let release = this.$root.$data.releaseIndex[product];
-          if (release && release.name) {
-              return release.name;
-           } else {
-              return product;
-          }
+            let product = this.$root.$data.product;
+            if (!product) {
+                return '';
+            }
+            let release = this.$root.$data.releaseIndex[product];
+            if (release && release.name) {
+                return release.name;
+            } else {
+                return product;
+            }
         },
+
+        installable() {
+            return this.$root.$data.product && this.$root.$data.releaseIndex[this.$root.$data.product].web_install
+        }
     },
 
     props: ["device", "blobStore", "active"],
