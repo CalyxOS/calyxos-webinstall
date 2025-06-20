ce18ad859d593f6c1f22040fc760146a99665641
ziggy <ziggy@calyxinstitute.org>
2025-06-20 11:27:34 -0400

    npm update
    Change-Id: I8a91c6c23686dc19a6c62448672a1744f04ae05c

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
 import path from "path"
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      fastboot: path.resolve(__dirname, "./lib/fastboot"),
-      opfs_blob_store: path.resolve(__dirname, "./lib/opfs_blob_store"),
     },
   },
   base: process.env.BASE_URL ? process.env.BASE_URL : "/",
