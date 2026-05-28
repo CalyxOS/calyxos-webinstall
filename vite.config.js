3f73d399271109e7f387181595322bb1fab0d345
ziggy <ziggy@calyx.org>
2026-05-28 12:26:34 -0400

    webinstall: Update packages
    Upgrade following packages:
    - typescript 6
    - vite 8
    - vuetify 4
    
    Replace eslint with oxlint
    Replace prettier with oxfmt
    
    Change-Id: If11ae6a3d09c0276d8d3c31c0c0b6308566d2f1b

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import vueDevTools from "vite-plugin-vue-devtools"
 import path from "path"
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
